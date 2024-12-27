ffa41b894ab99d5767d42f46002efc41db3eaef4
Revelin911 <revelin911@mail.com>
2024-12-26 19:56:53 -0700

    revised all of Truck.ts

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -5,110 +5,88 @@ import Car from './Car.js';
 import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
-// TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
 
-interface AbleToTow{
-towCapacity: number;
-tow(): void;
-}
-
- // TODO: Declare properties of the Truck class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
-  class Truck {
-    // Declare properties of the Truck class
+// TODO: Declare properties of the Truck class
+// TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+// TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+class Truck{
   vin: number;
   color: string;
   make: string;
   model: string;
- year: number;
- weight: number;
+  year: number;
+  weight: number;
   topSpeed: number;
   wheels: Wheel[];
-    // Constructor for the Truck class
-  // TODO: Create a constructor that accepts the properties of the Truck class
-  constructor (
-    vin: number,
-  color: string,
-  make: string,
-  model: string,
-  year: number,
-  weight: number,
-  topSpeed: number,
-  wheels: Wheel[]
-) {
-  this.vin = vin;
-  this.color = color;
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.weight = weight;
-  this.topSpeed = topSpeed;
-  this.wheels = wheels;
+  towingCapacity: number;
 }
 
-  
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
+class Vehicle {
+  constructor (public vin: number, public color: string, public make: string, public model: string, public year: number) {}
+  printDetails(): void {
+console.log(`VIN:${this.vin}, Make:${this.make}, Model:${this.model}, Year:${this.year}`);
+} 
+}
+
+interface AbleToTow {
+ tow(vehicle: Truck | Motorbike | Car): void;
+  towingCapacity: number;
+    
+}
+
+class Wheel {
+  constructor(public size: number) {}
+}
+
+// TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+class Truck extends Vehicle implements AbleToTow {
+  public weight: number;
+  public topSpeed: number;
+  public wheels: Wheel[];
+  public towingCapacity: number;
+
+  // TODO: Create a constructor that accepts the properties of the Truck class
+  // TODO: The constructor should call the constructor of the parent class, Vehicle
     // TODO: The constructor should initialize the properties of the Truck class
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-    class Truck extends Vehicle implements AbleToTow{
-  constructor(make: string, model: string, year: number, public towCapacity: number) {
-    super(make, model, year); // Call the constructor of the Vehicle class
-}
- }
+  constructor(vin: number, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[], towingCapacity: number) {
+    super(vin, color, make, model, year );
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.towingCapacity;
 
-  // TODO: Implement the tow method from the AbleToTow interface
-  tow(vehicle: Truck | Motorbike | Car): void {
-    // TODO: Get the make an model of the vehicle if it exists
-    // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
-    // TODO: If it is not, log that the vehicle is too heavy to be towed
+    if (wheels.length !== 4) {
+      this.wheels = Array(4).fill(null).map(() => new Wheel(16));
+    } else {
+      this.wheels = wheels;
+    }
   }
+}
+{
+    
+// TODO: Implement the tow method from the AbleToTow interface
+  // TODO: Get the make an model of the vehicle if it exists
+  // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+  // TODO: If it is, log that the vehicle is being towed
+  // TODO: If it is not, log that the vehicle is too heavy to be towed
+  tow(vehicle: Truck | Motorbike | Car): void {
+    if(this.weight <= this.towingCapacity) {
+      console.log(`${vehicle.make} ${vehicle.model} will be towed.`);
+    } else {
+      console.log(`${vehicle.make} ${vehicle.model} is too heavy to be towed.`);
+    }
+}
 
   // TODO: Override the printDetails method from the Vehicle class
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+printDetails(): void {
+  super.printDetails();
+  console.log(`Weight: ${this.weight}, Top Speed: ${this.topSpeed}, Color: ${this.color}, Towing Capacity: ${this.towingCapacity}, Wheels: ${this.wheels.length}`);
 }
-
-class Vehicle extends Truck{
-
 }
 
 // Export the Truck class as the default export
 export default Truck;
 
-
-
-// class Truck {
-//   vin: number;
-//   color: string;
-//   make: string;
-//   model: string;
-//  year: number;
-//  weight: number;
-//   topSpeed: number;
-//   wheels: Wheel[];
-
-// constructor (
-//   vin: number,
-// color: string,
-// make: string,
-// model: string,
-// year: number,
-// weight: number,
-// topSpeed: number,
-// wheels: Wheel[]
-// ) {
-// this.vin = vin;
-// this.color = color;
-// this.make = make;
-// this.model = model;
-// this.year = year;
-// this.weight = weight;
-// this.topSpeed = topSpeed;
-// this.wheels = wheels;
-// }
-
-//  displayInfo(): string {
-//   return `${this.year} ${this.make} ${model}`;
