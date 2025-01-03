23c4d511d625fa499fb04883c03a0b96f3b84be1
Revelin911 <revelin911@mail.com>
2025-01-02 17:01:09 -0700

    corrected motorbike class

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -32,13 +32,13 @@ class Motorbike extends Vehicle{
   super();
 
   // TODO: The constructor should initialize the properties of the Motorbike class
-this.vin = vin,
-this.color = color,
+this.vin = vin;
+this.color = color;
 this.make = make;
-this.model = model,
-this.year = year,
-this.weight = weight,
-this.topSpeed = topSpeed,
+this.model = model;
+this.year = year;
+this.weight = weight;
+this.topSpeed = topSpeed;
     
     // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
     if (wheels.length !== 2) {
@@ -50,19 +50,26 @@ this.topSpeed = topSpeed,
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-    wheelie(vehicle: Motorbike){
-
-    }
-    printDetails(): void {
-      super.printDetails();
-      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
-    }
-    }
-
+    wheelie(): void{
+console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }  
+  
   // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+
   // TODO: The method should call the printDetails method of the parent class
-  // TODO: The method should log the details of the Motorbike
+  super.printDetails();
+// TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  console.log(`VIN: ${this.vin}`);
+  console.log(`Make: ${this.make}`);
+  console.log(`Model: ${this.model}`);
+  console.log(`Year: ${this.year}`);
+  console.log(`Weight: ${this.weight} lbs`);
+  console.log(`Top Speed: ${this.topSpeed} mph`);
+  console.log(`Color: ${this.color}`);
+  console.log(`${this.wheels} wheels`);
+  }
 }
 
 // Export the Motorbike class as the default export
